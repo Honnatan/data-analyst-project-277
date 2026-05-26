88fba3ae20302caf5c1ab14658e10dd7e49e1c92
Honnatan <120799568+Honnatan@users.noreply.github.com>
2026-05-26 16:07:51 -0500

    Delete queries directory

--- queries/Scripts/day_of_the_week_income2.sql
+++
@@ -1,9 +0,0 @@
--- Este codigo genera una tabla con los ingresos de los vendedores por dia de la semana. 
-
-select	concat(e.first_name, ' ', e.last_name) as seller,
-		TO_CHAR(s.sale_date , 'Day') as day_of_week,
-		floor(sum(s.quantity)) as income
-from sales s
-join employees e 
-	on s.sales_person_id = e.employee_id
-group by concat(e.first_name, ' ', e.last_name),TO_CHAR(s.sale_date , 'Day');
--- queries/customers_count.sql
+++
@@ -1,3 +0,0 @@
--- Este comando cuenta el número total de clientes en la tabla customers
-select COUNT(customer_id) as customers_count from customers;
-
